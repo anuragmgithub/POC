6f2f05bf3d4675b19da368f83123b144b3a900bc
Anurag Mishra <mishra.anurag643@gmail.com>
2025-06-22 12:35:15 +0530

    sf test connectivity

--- snowflake/sf/src/conn.py
+++ snowflake/sf/src/conn.py
@@ -4,6 +4,19 @@ import snowflake.connector
 
 load_dotenv()
 
+# conn = snowflake.connector.connect(
+#     user=os.getenv("SNOWFLAKE_USER"),
+#     password=os.getenv("SNOWFLAKE_PASSWORD"),
+#     account=os.getenv("SNOWFLAKE_ACCOUNT"),
+#     warehouse=os.getenv("SNOWFLAKE_WAREHOUSE"),
+#     database=os.getenv("SNOWFLAKE_DATABASE"),
+#     schema=os.getenv("SNOWFLAKE_SCHEMA"),
+#     role=os.getenv("SNOWFLAKE_ROLE")
+# )
+
+
+
+# Connect to Snowflake
 conn = snowflake.connector.connect(
     user=os.getenv("SNOWFLAKE_USER"),
     password=os.getenv("SNOWFLAKE_PASSWORD"),
@@ -13,7 +26,6 @@ conn = snowflake.connector.connect(
     schema=os.getenv("SNOWFLAKE_SCHEMA"),
     role=os.getenv("SNOWFLAKE_ROLE")
 )
-
 
 # Create a cursor object.
 cur = conn.cursor()
