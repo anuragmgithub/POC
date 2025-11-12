d7ce9fcfc80062983c4fda8d8ad3e409b21e94cb
Anurag Mishra <mishra.anurag643@gmail.com>
2025-11-12 22:47:12 +0530

    kafka producer

--- kafka/test_producer.py
+++ kafka/test_producer.py
@@ -2,6 +2,7 @@ from kafka import KafkaProducer
 import json
 
 # Initialize the Kafka producer
+# kafka producer
 producer = KafkaProducer(
     bootstrap_servers='localhost:9092',
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
